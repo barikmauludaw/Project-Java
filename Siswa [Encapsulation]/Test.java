ed7e209a0620db47cefeab8f179f6e4cf8dd4308
Barik Mauluda Wildani <116042437+barikmauludaw@users.noreply.github.com>
2023-06-11 08:33:07 +0700

    Add files via upload

---
+++ Siswa [Encapsulation]/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+      Students s1 = new Students();
+      s1.setName("Enkapsulasi");
+      s1.setMark(90);
+      System.out.println("s1Name is " + s1.getName());
+      System.out.println("s1Mark is " + s1.getMark());
+    }
+  }
